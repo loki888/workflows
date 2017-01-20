3b2b463b1cd35359376523fa279da1bd92a2c70b
Sergiy Sydorenko <loki_888@i.ua>
2017-01-20 21:42:55 +0000

    Add first gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var coffee = require("gulp-coffee");
 
+var coffeeSourses = ['components/coffee/tagline.coffee'];
 
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+gulp.task('coffee', function(){
+    gulp.src(coffeeSourses)
+    .pipe(coffee({bare:true})
+        .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'));
 });
