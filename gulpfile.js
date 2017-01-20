64cfc5a7a153482d7bbe85fd6691367fd1bb2415
Sergiy Sydorenko <loki_888@i.ua>
2017-01-20 22:23:42 +0000

    Add concat compile from sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var coffee = require("gulp-coffee");
 var browserify = require("gulp-browserify");
+var compass = require("gulp-compass");
 var concat = require("gulp-concat");
 
 var coffeeSourses = ['components/coffee/tagline.coffee'];
+
 var jsSourses = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
     ];
+    
+var sassSourses = [
+     'components/sass/style.scss'
+    ];
+    
 gulp.task('coffee', function(){
     gulp.src(coffeeSourses)
     .pipe(coffee({bare:true})
@@ -23,4 +30,15 @@ gulp.task('js', function(){
         .pipe(concat('script.js')) // final file name
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function(){
+    gulp.src(sassSourses)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        })) // final file name
+            .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'));
 });
